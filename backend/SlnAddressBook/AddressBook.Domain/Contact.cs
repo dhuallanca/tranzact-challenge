1b8a455cb66b3ef3704768f91fa6ef8bae0ebb31
Dennis Huallanca <dzavaleta@hotmail.com>
2021-09-26 18:53:23 -0500

    feat(backend): create repository pattern

---
+++ backend/SlnAddressBook/AddressBook.Domain/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Domain
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [EmailAddress(ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ backend/SlnAddressBook/AddressBook.Repository.EF/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using AddressBook.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Repository.EF
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
