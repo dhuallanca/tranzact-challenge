772027680e09dc37019da0f99e27648c7b956a9d
Dennis Huallanca <dzavaleta@hotmail.com>
2021-09-29 16:26:17 -0500

    feat(contact): create contact controller

---
+++ backend/SlnAddressBook/AddressBookApi.Tests/ContactsControllerTest.cs
@@ -0,0 +1,122 @@
+﻿using AddressBook.Domain;
+using AddressBook.Domain.Interfaces;
+using AddressBookApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AddressBookApi.Tests
+{
+    public class ContactsControllerTest
+    {
+        private ContactsController _contactController;
+        private readonly Mock<IContactRepository> _contactRepository;
+        public ContactsControllerTest()
+        {
+            _contactRepository = new Mock<IContactRepository>();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _contactController = new ContactsController(_contactRepository.Object);
+        }
+
+        [Test]
+        public async Task GetById_ContactExists_ReturnContact()
+        {
+            var contact = new Contact
+            {
+                ContactId = 1,
+                FirstName = "Dennis",
+                LastName = "Huallanca",
+                Address = "main avenue 135",
+                Phone = "",
+                Email = "test@domain.com"
+            };
+            const int id = 1;
+            _contactRepository.Setup(contactRepository => contactRepository.GetById(It.IsAny<int>())).ReturnsAsync(contact);
+
+            var actionResult = await _contactController.GetById(id);
+            var result = actionResult.Result as OkObjectResult;
+            Assert.AreEqual(contact, result.Value);
+        }
+
+        [Test]
+        public async Task GetById_ContactDoesNotExists_ReturnNotFound()
+        {
+            Contact contact = null;
+            const int id = 2;
+            _contactRepository.Setup(contactRepository => contactRepository.GetById(It.IsAny<int>())).ReturnsAsync(contact);
+
+            var actionResult = await _contactController.GetById(id);
+            var result = actionResult.Result as NotFoundResult;
+            Assert.IsTrue(result.StatusCode == 404);
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Get_ContactExists_ReturnContacts()
+        {
+            var contacts = new List<Contact> {
+                new Contact {
+                ContactId = 1,
+                FirstName = "Dennis",
+                LastName = "Huallanca",
+                Address = "main avenue 135",
+                Phone = "",
+                Email = "test@domain.com"
+                },
+                new Contact {
+                ContactId = 2,
+                FirstName = "Dennis",
+                LastName = "Huallanca",
+                Address = "main avenue 135",
+                Phone = "",
+                Email = "test@domain.com"
+                }
+            };
+
+            _contactRepository.Setup(contactRepository => contactRepository.GetAll()).ReturnsAsync(contacts);
+
+            var actionResult = await _contactController.Get();
+            var result = actionResult.Result as OkObjectResult;
+            var expectedContacts = (List<Contact>) result.Value;
+            CollectionAssert.AreEqual(expectedContacts, contacts);
+            Assert.AreEqual(expectedContacts.Count, contacts.Count);
+        }
+
+        [Test]
+        public async Task Post_ContactNull_ReturnBadRequest()
+        {
+            Contact contact = null;
+            _contactRepository.Setup(contactRepository => contactRepository.Insert(It.IsAny<Contact>()));
+
+            var actionResult = await _contactController.Post(contact);
+            var result = actionResult as BadRequestResult;
+            Assert.IsTrue(result.StatusCode == 400);
+            Assert.IsInstanceOf<BadRequestResult>(result);
+        }
+
+        [Test]
+        public async Task Post_ValidContact_ReturnSuccess()
+        {
+            Contact contact = new()
+            { 
+                FirstName = "Dennis",
+                LastName = "Hualllanca",
+                Address ="lima downtown"
+             };
+            _contactRepository.Setup(contactRepository => contactRepository.Insert(It.IsAny<Contact>()));
+
+            var actionResult = await _contactController.Post(contact);
+            
+            var result = actionResult as OkResult;
+            Assert.IsTrue(result.StatusCode == 200);
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+
+    }
+}
---
+++ backend/SlnAddressBook/AddressBookApi/Controllers/ContactsController.cs
@@ -0,0 +1,97 @@
+﻿using AddressBook.Domain;
+using AddressBook.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AddressBookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactRepository _contactRepository;
+
+        public ContactsController(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+        // GET: api/<ContactsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> Get()
+        {
+            var contacts = await _contactRepository.GetAll();
+            return Ok(contacts);
+        }
+
+        // GET api/<ContactsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetById(int id)
+        {
+            var contact = await _contactRepository.GetById(id);
+            return contact != null ? Ok(contact) : NotFound();
+        }
+
+        // POST api/<ContactsController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Contact contact)
+        {
+            try
+            {
+                if (contact == null)
+                {
+                    return BadRequest();
+                }
+                await _contactRepository.Insert(contact);
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500, "Error adding data contact");
+            }
+        }
+
+        // PUT api/<ContactsController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Contact contact)
+        {
+            if (contact.ContactId != id)
+            {
+                return BadRequest("");
+            }
+            try
+            {
+                await _contactRepository.Update(contact);
+                return Ok();
+
+            } 
+            catch
+            {
+                return StatusCode(500, "Error updating data contact");
+            }
+            
+        }
+
+        // DELETE api/<ContactsController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var contact = await _contactRepository.GetById(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                await _contactRepository.Delete(contact);
+                return Ok();
+            }
+            catch 
+            {
+                return StatusCode(500, "Error removing data contact");
+            }
+        }
+    }
+}
